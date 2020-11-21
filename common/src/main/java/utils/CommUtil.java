c58993484006475f2886eaee6643c6597cf6a334
zcg1011713962 <56151248+zcg1011713962@users.noreply.github.com>
2020-11-21 17:06:07 +0800

    curator3.x|zk3.5.6

--- common/src/main/java/utils/CommUtil.java
+++ common/src/main/java/utils/CommUtil.java
@@ -9,7 +9,7 @@ public class CommUtil {
     public static String getZookeeperServerList(){
         StringJoiner join = new StringJoiner(",");
         join.add("192.168.117.200:2181");
-       join.add("192.168.117.200:2182");
+        join.add("192.168.117.200:2182");
         join.add("192.168.117.200:2183");
         return join.toString();
     }
